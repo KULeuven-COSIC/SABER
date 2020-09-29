b004c6abfb56efd01df9836f6d96088ce74ace0b
Michiel Van Beirendonck <michiel.vanbeirendonck@esat.kuleuven.be>
2020-09-29 13:33:22 +0200

    Remove Kyber ref from new poly.c

--- Reference_C/poly.c
+++ Reference_C/poly.c
@@ -1,10 +1,3 @@
-/*---------------------------------------------------------------------
-This file has been adapted from the implementation 
-(available at, Public Domain https://github.com/pq-crystals/kyber) 
-of "CRYSTALS – Kyber: a CCA-secure module-lattice-based KEM"
-by : Joppe Bos, Leo Ducas, Eike Kiltz, Tancrede Lepoint, 
-Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
-----------------------------------------------------------------------*/
 #include <stdio.h>
 #include "api.h"
 #include "poly.h"
--- Reference_C/poly.h
+++ Reference_C/poly.h
@@ -1,11 +1,3 @@
-/*---------------------------------------------------------------------
-This file has been adapted from the implementation 
-(available at, Public Domain https://github.com/pq-crystals/kyber) 
-of "CRYSTALS – Kyber: a CCA-secure module-lattice-based KEM"
-by : Joppe Bos, Leo Ducas, Eike Kiltz, Tancrede Lepoint, 
-Vadim Lyubashevsky, John M. Schanck, Peter Schwabe & Damien stehle
-----------------------------------------------------------------------*/
-
 #ifndef POLY_H
 #define POLY_H
 
